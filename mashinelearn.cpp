e4523e7b689f224e8141b08ffab371a924bef715
Marina <marimysh@gmail.com>
2015-05-29 21:32:35 +0300

    begin logic regress

--- mashinelearn.cpp
+++ mashinelearn.cpp
@@ -2,12 +2,37 @@
 
 MashineLearn::MashineLearn()
 {
-	//this->weigt.resize (28,0);
+	this->weight.resize (28,0);
 	for (size_t i = 0; i < 28; ++i)
 		for (size_t j = 0; j < 28; ++j)
-			this->weigt[i][j] = 0;
+			this->weight[i][j] = 0;
 	target = 0;
 	eta = 1;
+}
+
+double MashineLearn::Pmiss (bool y, std::vector<double>::iterator input)
+{
+	double sum = 0;
+	for (size_t i = 0; i < input.size(); ++i)
+		sum += input.at(i) * this.weight.at(i);
+	if (y)
+		return (exp(-sum) / (1 + exp(-sum)));
+	else
+		return (exp(sum) / (1 + exp(sum)));
+}
+
+void MashineLearn::LogicRegression(std::vector<double>::iterator input)
+{
+	for (size_t i = 0; i < input.size(); ++i)
+	{
+		int number = input.at(0);
+		++input;
+		double Ptrue = Pmiss(true, input);
+		double Pfalse = Pmiss(false, input);
+		for (size_t j = 0; j < 10; ++j)
+			if (i == number)
+				this.weight.at(j).at(i) += Pmiss(y, input);
+	}
 }
 
 MashineLearn::~MashineLearn()
@@ -15,7 +40,7 @@ MashineLearn::~MashineLearn()
 
 }
 
-double MashineLearn::ThisIsNumber (std::vector<std::vector<double> >::iterator input)
+double MashineLearn::ThisIsNumber (std::vector<double>::iterator input)
 {
 	
 }
